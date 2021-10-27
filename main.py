9ce69eec2af6512f259b6347314cb1e723b6e00a
Viv Hafener <79525734+viv-codes@users.noreply.github.com>
2021-10-27 10:52:31 -0400

    Moved most of my main code from main to working

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask
+import flask
+from flask.templating import render_template
+from flask import Flask, request, flash, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///journal.sqlite3'
+app.config['SECRET_KEY'] = "random string"
+
+db = SQLAlchemy(app)
+
+class journal(db.Model):
+    id = db.Column('entry_id', db.Integer, primary_key = True)
+    time = db.Column(db.String(50))
+    title = db.Column(db.String(100))
+    entry = db.Column(db.String(200))
+
+    def __init__(self, time, title, entry):
+        self.time = time
+        self.title = title
+        self.entry = entry
+
+@app.route('/')
+def show_all():
+    return render_template('show_all.html', journal = journal.query.all() )
+
+@app.route('/new', methods = ['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['title'] or not request.form['time'] or not request.form['entry']:
+            flash('Please enter something in each field', 'error')
+        else:
+            jrnl_entry = journal(request.form['title'], request.form['time'], request.form['entry'])
+
+            db.session.add(jrnl_entry)
+            db.session.commit()
+            flash('Entry added to journal')
+            return redirect(url_for('show_all'))
+    return render_template('new.html')
+
+# Should I move this over to the new one?
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
--- project/main.py
+++ project/main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, url_for, redirect, 
 from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
@@ -14,7 +14,23 @@ def index():
 def profile():
     return render_template('profile.html', name=current_user.name)
 
+# Login stuff above this line, journal stuff below this line
+
 @main.route('/home')
 @login_required
 def homepage():
-    return render_template('show_all.html', name=current_user.name) # show_all doesn't use name yet but I'll add it. Also need to figure out how to only access the data from the individual user
+    return render_template('show_all.html', name=current_user.name, journal = journal.query.all() ) # show_all doesn't use name yet but I'll add it. Also need to figure out how to only access the data from the individual user
+
+@main.route('/new', methods = ['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['title'] or not request.form['time'] or not request.form['entry']:
+            flash('Please enter something in each field', 'error')
+        else:
+            jrnl_entry = journal(request.form['title'], request.form['time'], request.form['entry'])
+
+            db.session.add(jrnl_entry)
+            db.session.commit()
+            flash('Entry added to journal')
+            return redirect(url_for('show_all'))
+    return render_template('new.html')
