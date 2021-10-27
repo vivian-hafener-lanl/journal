82a574b93b5a2488ec05a403c7e13d006a22880a
Viv Hafener <79525734+viv-codes@users.noreply.github.com>
2021-10-27 10:47:52 -0400

    Started to re-integrate login with my code

--- project/main.py
+++ project/main.py
@@ -13,3 +13,8 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/home')
+@login_required
+def homepage():
+    return render_template('show_all.html', name=current_user.name) # show_all doesn't use name yet but I'll add it. Also need to figure out how to only access the data from the individual user
