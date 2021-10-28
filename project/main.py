e526fbbe124278be92c1b2bbb2e1c83e50e60943
viv-codes <79525734+viv-codes@users.noreply.github.com>
2021-10-27 22:43:17 -0400

    Did some routing fixes, added a logout button to the homepage

--- project/main.py
+++ project/main.py
@@ -10,12 +10,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
-
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', name=current_user.name)
+    return redirect(url_for('main.home'))
 
 # Login stuff above this line, journal stuff below this line
 
