8998064ca1782034fda535df42834a7c8a054f30
viv-codes <79525734+viv-codes@users.noreply.github.com>
2021-11-02 00:24:58 -0400

    Running into issues with current_user.id not ending up in the database. It is available in the code at the time of calling, but isn't being put in the db

--- project/main.py
+++ project/main.py
@@ -15,7 +15,7 @@ def index():
 @main.route('/home')
 @login_required
 def home():
-    return render_template('home.html', name=current_user.name, journal = journal.query.all() ) # show_all doesn't use name yet but I'll add it. Also need to figure out how to only access the data from the individual user
+    return render_template('home.html', name=current_user.name, u_id = current_user.id, journal = journal.query.all() ) # show_all doesn't use name yet but I'll add it. Also need to figure out how to only access the data from the individual user
 
 @main.route('/new', methods = ['GET', 'POST'])
 @login_required
@@ -23,8 +23,10 @@ def new():
     if request.method == 'POST':
         if not request.form['title'] or not request.form['time'] or not request.form['entry']:
             flash('Please enter something in each field', 'error')
+
         else:
-            jrnl_entry = journal(current_user.id, request.form['title'], request.form['time'], request.form['entry'])
+            user_identifier = current_user.id
+            jrnl_entry = journal(user_identifier, request.form['title'], request.form['time'], request.form['entry'])
 
             jrnl_db.session.add(jrnl_entry)
             jrnl_db.session.commit()
